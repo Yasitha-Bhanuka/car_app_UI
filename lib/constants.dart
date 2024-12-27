3e37e697a5afdb10c53cf6e2d7cb75aeffde9174
Yasitha Bhanuka <yasithabhanukac@gmail.com>
2024-12-27 07:39:55 +0530

    Initialize Flutter project with Android setup and basic structure and background color for MaterialApp using constant.dart

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'dart:ui';
+
+Color backgroundColor = const Color(0xFF40ac9c);
+Color cardColor = const Color(0xFF203e5a);
---
+++ lib/screens/Home/car_home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:car_app/constants.dart';
+import 'package:flutter/material.dart';
+
+class CarHomeScreen extends StatelessWidget {
+  const CarHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+    );
+  }
+}
